e19eb0bf3d12bc21e9c49de095753dcd46fa2a9d
tuxedotshirt <don.hagen@hotmail.com>
2021-08-10 19:02:45 -0600

    Introduction to Room and Flow step 4 complete

---
+++ app/src/main/java/database/schedule/Schedule.kt
@@ -0,0 +1,9 @@
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity
+data class Schedule(
+    @PrimaryKey val id: Int,
+    @NonNull @ColumnInfo(name = "stop_name") val stopName: String,
+    @NonNull @ColumnInfo(name = "arrival_time") val arrivalTime: Int
+)
